4b80183f00fb4b591823dd668392055e3ab39a74
ken <glow_hiatus0w@icloud.com>
2021-08-04 19:39:26 +0800

    added trigger

--- custom-sdk/raspberry-pi-sensehat-sdk.rb
+++ custom-sdk/raspberry-pi-sensehat-sdk.rb
@@ -1,54 +1,98 @@
 {
   title: 'Raspberry Pi SenseHAT',
   secure_tunnel: true,
-  
+
   connection: {
     fields: [{ name: 'profile', hint: 'On-prem Raspberry Pi SenseHAT connection profile' }],
     authorization: { type: 'none'}
   },
-  
+
   test: lambda do |connection|
     get("http://localhost/ext/#{connection['profile']}/ping").headers('X-Workato-Connector': 'enforce')
   end,
-  
-  actions: {
-    
-    clear: {
-      title: 'Clear LED Matrix',
-      description: 'Clear the LED Matrix display.',
-      
-      output_fields: lambda do |object_definitions| 
-        [{name: 'status', type: :string}]
-      end,
-      
-      execute: lambda do |connection|
-        get("http://localhost/ext/#{connection['profile']}/clear").headers('X-Workato-Connector': 'enforce')
-      end
-    },
-    
+
+  triggers: {
+
     environmentalSensor: {
-      title: 'Get environmental sensor reading',
-      description: 'Get humidity, temperature (from two sensors) and pressure reading from the SenseHAT enviromental sensor.',
-      
-      output_fields: lambda do |object_definitions| 
+      title: 'New environmental sensor reading',
+      description: lambda do |input, picklist_label|
+        'Poll for humidity, temperature (from two sensors) and pressure reading from the SenseHAT enviromental sensor.'
+      end,
+
+      poll: lambda do |connection|
+        readings = get("http://localhost/ext/#{connection['profile']}/environmentalSensor").headers('X-Workato-Connector': 'enforce')
+
+        {
+          events: readings,
+          can_poll_more: false
+        }
+      end,
+
+      dedup: lambda do |readings|
+        "#{readings['humidity']+readings['temperature']+readings['temperatureFromPressure']+readings['pressure']}"
+      end,
+
+      output_fields: lambda do |object_definitions|
         [
-          {name: 'humidity', type: :float}, 
+          {name: 'humidity', type: :float},
           {name: 'temperature', type: :float},
           {name: 'temperatureFromPressure', type: :float},
           {name: 'pressure', type: :float}
         ]
       end,
-      
+
+      sample_output: lambda do |connection|
+        {
+          temperature: 41.995728,
+          humidity: 50.755486,
+          temperatureFromPressure: 38.972916,
+          pressure: 1011.19385
+        }
+      end,
+
+
+    },
+
+  },
+
+  actions: {
+
+    clear: {
+      title: 'Clear LED Matrix',
+      description: 'Clear the LED Matrix display.',
+
+      output_fields: lambda do |object_definitions|
+        [{name: 'status', type: :string}]
+      end,
+
+      execute: lambda do |connection|
+        get("http://localhost/ext/#{connection['profile']}/clear").headers('X-Workato-Connector': 'enforce')
+      end
+    },
+
+    environmentalSensor: {
+      title: 'Get environmental sensor reading',
+      description: 'Get humidity, temperature (from two sensors) and pressure reading from the SenseHAT enviromental sensor.',
+
+      output_fields: lambda do |object_definitions|
+        [
+          {name: 'humidity', type: :float},
+          {name: 'temperature', type: :float},
+          {name: 'temperatureFromPressure', type: :float},
+          {name: 'pressure', type: :float}
+        ]
+      end,
+
       execute: lambda do |connection|
         get("http://localhost/ext/#{connection['profile']}/environmentalSensor").headers('X-Workato-Connector': 'enforce')
       end
     },
-    
+
     displayMessage: {
       title: 'Display message',
       description: 'Display a message on the SenseHAT LED Matrix',
-      
-      input_fields: lambda do |object_definitions| 
+
+      input_fields: lambda do |object_definitions|
         [
           {name: 'message', label: 'Message', type: :string, optional: false},
           {name: 'messageColor', label: 'Message Color', type: :string, pick_list: 'colors', control_type: 'select', optional: false},
@@ -58,71 +102,71 @@
       output_fields: lambda do |object_definitions|
         [{name: 'status'}]
       end,
-      
+
       execute: lambda do |connection, input|
         post("http://localhost/ext/#{connection['profile']}/displayMessage", input).headers('X-Workato-Connector': 'enforce')
       end
     },
-    
+
     displayLetter: {
       title: 'Display a letter',
       description: 'Display a letter on the SenseHAT LED Matrix',
-      
+
       input_fields: lambda do |object_definitions|
-        [{name: 'letter', label: 'Letter', type: :string, optional: false}, 
+        [{name: 'letter', label: 'Letter', type: :string, optional: false},
         {name: 'letterColor', label: 'Letter Color', type: :string, pick_list: 'colors', control_type: 'select', optional: false},
         {name: 'backgroundColor', label: 'Background Color', type: :string, pick_list: 'colors', control_type: 'select', optional: false}]
-      end, 
-      output_fields: lambda do |object_definitions|
-        [{name: 'status'}] 
       end,
-        
+      output_fields: lambda do |object_definitions|
+        [{name: 'status'}]
+      end,
+
       execute: lambda do |connection, input|
         post("http://localhost/ext/#{connection['profile']}/displayLetter", input).headers('X-Workato-Connector': 'enforce')
       end
     },
-      
+
     color: {
       title: 'Set LED Color',
       description: 'Display an color based on the RGB value on the SenseHAT LED Matrix',
-      
+
       input_fields: lambda do |object_definitions|
         [{name: 'color', label: 'Color', type: :string, control_type: 'select', pick_list: 'colors', optional: false}]
       end,
       output_fields: lambda do |object_definitions|
         [{name: 'status'}]
       end,
-      
+
       execute: lambda do |connection, input|
         post("http://localhost/ext/#{connection['profile']}/displayColor", input).headers('X-Workato-Connector': 'enforce')
       end
     },
-    
+
     displayStopIcon: {
       title: 'Display a stop sign',
       description: 'Display a stop sign icon on the SenseHAT LED Matrix',
-      
+
       output_fields: lambda do |object_definitions|
         [{name: 'status'}]
       end,
-      
+
       execute: lambda do |connection, input|
         get("http://localhost/ext/#{connection['profile']}/displayStopIcon", input).headers('X-Workato-Connector': 'enforce')
       end
     },
-    
+
     displayCheckIcon: {
       title: 'Display a check sign',
       description: 'Display a check sign icon on the SenseHAT LED Matrix',
-      
+
       output_fields: lambda do |object_definitions|
         [{name: 'status'}]
       end,
-      
+
       execute: lambda do |connection, input|
         get("http://localhost/ext/#{connection['profile']}/displayCheckIcon", input).headers('X-Workato-Connector': 'enforce')
       end
-    } 
+    }
 
   },
   pick_lists: {
